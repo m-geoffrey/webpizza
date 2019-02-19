2c5bc8fda99c21453eba31f654c3647356dfd09c
Moniez Geoffrey <moniez.geoffrey@yahoo.fr>
2019-02-19 15:09:59 +0100

    -

---
+++ private/config/config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Fichier de configuration general de l'application
+ * 
+ * 1. Définition des constantes
+ * 2. Définition des variables d'environnement d'exécution
+ * 3. Définition 
+ */
+
+/**
+ * 1. Définition des constantes
+ */
+
+// WEBSITE_TITLE : Définition du titre du site
+define ('WEBSITE_TITLE', "WebPizza !");
+
+/**
+ * 2. Définition des variables d'environnement d'exécution
+ */
+
+ // Environnement de développement ou production ?
+ // Les valeurs peuvent être : "prod" ou "dev"
+//  Par défaut on considère que l'application s'exécute en environnement de PROD
+$env = "prod";
+
+ // Liste des domaines que l'on considère comme étant des environnements de développement
+$dev_domains = [
+    "127.0.0.1",
+    "localhost",
+    "webpizza.local"
+];
+
+
